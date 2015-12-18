e1213e8f84cdb613730cbd841c629ebc10295aca
johnsmithm <moshnoi2000@gmail.com>
2015-12-18 12:30:05 +0100

    make file

--- ex03/cg.cpp
+++ ex03/cg.cpp
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 
 
+#include <mpi.h>
 #include"Vector.h"  //Expr_CG function
-#include "cg_naive.h" //CG class
+//#include "cg_naive.h" //CG class
 
-#include "CG.h"
 #include "Timer.h"
 
 #ifdef USE_LIKWID
@@ -21,11 +21,19 @@ using namespace std;
 
 int main(int argc, char *argv[]){
     (void) argc; //to suppress Warnings about unused argc
+   int size(0); 
+    int rank(0);
+	
+	MPI_Init( &argc, &argv );
+	
+	MPI_Comm_size( MPI_COMM_WORLD, &size );
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	
     assert(argc>3);
-    int nx   = atoi(argv[1]);
-    int ny   = atoi(argv[2]);
-    int c    = atoi(argv[3]);	
-    int eps  = stod(argv[4]);
+    int nx = atoi(argv[1]);
+    int ny = atoi(argv[2]);
+    int c  = atoi(argv[3]);	
+    double eps  = stod(argv[4]);
     
 #ifdef USE_LIKWID
    likwid_markerInit();
@@ -36,11 +44,11 @@ int main(int argc, char *argv[]){
     siwir::Timer timer;
     
     //using simple function operation on vectors
-    CG fast(nx,ny,c,eps);
-    double  r = fast.solve_naive();
+    //CG fast(nx,ny,c,eps);
+    //double  r = fast.solve_naive();
 	
 	//using template expretions
-    //double r = Expr_CG(nx,ny,c,eps);
+    double r = Expr_CG(nx,ny,c,eps);
     
     double time = timer.elapsed();
 
@@ -49,9 +57,10 @@ int main(int argc, char *argv[]){
    likwid_markerClose();
 #endif
 
-   fast.print_gnuplot();
-   cout<<"time:"<<time<<'\n';
-   cout<<"R:"<<r<<'\n';
+   if(rank==0){
+    cout<<"time:"<<time<<'\n';
+    cout<<"R:"<<r<<'\n';
+	}
 
 
 }
